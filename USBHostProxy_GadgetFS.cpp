e19a1b068a17095fac110aeb758a2d3edf788a03
Adam Stasiak <palesius@gmail.com>
2013-11-25 14:31:07 -0500

    add some error handling on open/write

--- USBHostProxy_GadgetFS.cpp
+++ USBHostProxy_GadgetFS.cpp
@@ -126,12 +126,18 @@ int USBHostProxy_GadgetFS::connect(USBDevice* device) {
 	strcat(path, device_filename);
 
 	p_device_file = open(path, O_RDWR);
-	if (p_device_file < 0)
+	TRACE1(p_device_file);
+	if (p_device_file < 0) {
 		fprintf(stderr,"Fail on open %d %s\n",errno,strerror(errno));
+		return 1;
+	}
 
 	status = write(p_device_file, descriptor_buf, ptr - descriptor_buf);
-	if (status < 0)
+	if (status < 0) {
 		fprintf(stderr,"Fail on write %d %s\n",errno,strerror(errno));
+		close(p_device_file);
+		return 1;
+	}
 
 	p_is_connected = true;
 	return 0;
@@ -139,6 +145,7 @@ int USBHostProxy_GadgetFS::connect(USBDevice* device) {
 
 void USBHostProxy_GadgetFS::disconnect() {
 	//FINISH
+	TRACE1(p_device_file);
 	if (!p_is_connected) {fprintf(stderr,"GadgetFS not connected.\n"); return;}
 	
 	//FINISH - check if it's open
