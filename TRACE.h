98c6c5504b2e1cf5ebf371a0270a507776bdb25e
Adam Stasiak <palesius@gmail.com>
2013-11-25 12:48:30 -0500

    added stdio to trace.h

--- TRACE.h
+++ TRACE.h
@@ -26,6 +26,8 @@
 #ifndef TRACE_H_
 #define TRACE_H_
 
+#include <stdio.h>
+
 #define TRACE fprintf(stderr,"Trace: %s, line %d\n",__FILE__,__LINE__);
 #define TRACE1(X) fprintf(stderr,"Trace(%d): %s, line %d\n",X,__FILE__,__LINE__);
 #define TRACE2(X,Y) fprintf(stderr,"Trace(%d,%d): %s, line %d\n",X,Y,__FILE__,__LINE__);
