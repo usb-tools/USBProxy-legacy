7804cb3cf2791495d26fbbdb7bf62d11c2e71431
Dominic Spill <dominicgs@gmail.com>
2014-07-12 18:54:49 +0100

    Add debian package build files

---
+++ src/debian/header-check.c
@@ -0,0 +1,43 @@
+/* header-check.c
+ *
+ * Test program to ensure all required headers are in the debian package,
+ * by Laszio <ezerotven@gmail.com>
+ * modified by Dominic Spill <dominicgs@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <USBProxy/ConfigParser.h>
+#include <USBProxy/DefinitionErrors.h>
+#include <USBProxy/DeviceQualifier.h>
+#include <USBProxy/HaltSignal.h>
+#include <USBProxy/Injector.h>
+#include <USBProxy/PacketFilter.h>
+#include <USBProxy/PluginManager.h>
+#include <USBProxy/TCP_Helper.h>
+#include <USBProxy/Configuration.h>
+#include <USBProxy/Device.h>
+#include <USBProxy/Endpoint.h>
+#include <USBProxy/HexString.h>
+#include <USBProxy/Interface.h>
+#include <USBProxy/Plugins.h>
+#include <USBProxy/Criteria.h>
+#include <USBProxy/DeviceProxy.h>
+#include <USBProxy/get_tid.h>
+#include <USBProxy/HostProxy.h>
+#include <USBProxy/Manager.h>
+#include <USBProxy/Packet.h>
+#include <USBProxy/Proxy.h>
+#include <USBProxy/USBString.h>
