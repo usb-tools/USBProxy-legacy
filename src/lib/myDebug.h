08adf37a0a719851abd6bbf131656b5d0000507c
Kiyotaka ATSUMI <atsumi@aizulab.com>
2014-09-04 08:36:32 +0000

    modify myDebug.h

--- src/lib/myDebug.h
+++ src/lib/myDebug.h
@@ -14,18 +14,7 @@ extern void myDump( void*, int);
 }
 #endif
 
-//#ifdef DEBUG
-#define dbgTrace() fprintf( stderr, "%s %s() %d\n", __FILE__, __func__, __LINE__)
 #define dbgMessage(str) fprintf( stderr, "%s %s() %d %s\n", __FILE__, __func__, __LINE__, str)
-// #define return if ( ( fprintf( stderr, "return@%s:%s()#%d\n", __FILE__, __func__, __LINE__), false)) {} else return
-//#else
-//#define dbgTrace()
-//#define dbgMessage(str)
-//#define return return
-//#endif // DEBUG
-
-#define TEST(expr) (((expr)) ? ( fprintf( stderr, "%s: %d %s: `%s' failed.\n", __FILE__, __LINE__, __func__, __STRING(expr)), false) : true)
-
-#define assert2(expr) (((expr)) ? false : ( fprintf( stderr, "%s: %d %s: `%s' failed.\n", __FILE__, __LINE__, __func__, __STRING(expr)), abort(), true))
+#define IS_FAILED(expr) (((expr)) ? false : ( fprintf( stderr, "%s: %d %s: `%s' failed.\n", __FILE__, __LINE__, __func__, __STRING(expr)), true))
 
 #endif // _MYDEBUG_H
