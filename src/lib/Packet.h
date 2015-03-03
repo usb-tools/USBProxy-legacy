c08290a57fa6704d5bce806f6bd7a5e2ead15218
Alexander Holler <holler@ahsoftware.de>
2015-03-03 17:47:22 +0100

    Make SetupPacket a Packet

--- src/lib/Packet.h
+++ src/lib/Packet.h
@@ -5,10 +5,13 @@
 #ifndef USBPROXY_PACKET_H
 #define USBPROXY_PACKET_H
 
+#include <memory> // shared_ptr
+
 #include <stdlib.h>
 #include <linux/usb/ch9.h>
 
-struct Packet {
+class Packet {
+public:
 	__u8	bEndpoint;
 	__u16	wLength;
 	bool	filter;
@@ -16,20 +19,28 @@ struct Packet {
 	__u8*	data;
 
 	Packet(__u8 _endpoint,__u8* _data,__u16 _length,bool _filter=true) : bEndpoint(_endpoint),wLength(_length),filter(_filter),transmit(true),data(_data) {}
-	~Packet() {if (data) {free(data);data=NULL;}}
+	virtual ~Packet() {if (data) {free(data);data=NULL;}}
 };
 
-struct SetupPacket {
+class SetupPacket : public Packet {
+public:
 	usb_ctrlrequest ctrl_req;
 	int				source;
+	// TODO: share more fields with Packet
 	bool			filter_out;
 	bool 			transmit_out;
 	bool			filter_in;
 	bool			transmit_in;
-	__u8*			data;
 
-	SetupPacket(usb_ctrlrequest _ctrl_req,__u8* _data,bool _filter=true) : ctrl_req(_ctrl_req),source(0),filter_out(_filter),transmit_out(true),filter_in(_filter),transmit_in(true),data(_data) {}
-	~SetupPacket() {if (data) {free(data);data=NULL;}}
+	SetupPacket(usb_ctrlrequest _ctrl_req,__u8* _data,bool _filter=true)
+		: Packet(0, _data, 0, _filter)
+		, ctrl_req(_ctrl_req)
+		, source(0)
+		, filter_out(_filter)
+		, transmit_out(true)
+		, filter_in(_filter)
+		, transmit_in(true)
+	{}
 };
 
 #endif /* USBPROXY_PACKET_H */
