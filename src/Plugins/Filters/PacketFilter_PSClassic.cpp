61ef00e5a09237656be2ef2f0a2b4e89078dd743
Christopher J. Mallery <christopher.mallery@gmail.com>
2019-07-26 10:54:25 -0700

    try agian

--- src/Plugins/Filters/PacketFilter_PSClassic.cpp
+++ src/Plugins/Filters/PacketFilter_PSClassic.cpp
@@ -11,22 +11,22 @@ PacketFilter_PSClassic::PacketFilter_PSClassic(ConfigParser *cfg) {
 void PacketFilter_PSClassic::filter_packet(Packet* packet) {
 	if (packet->wLength >= 2) {
 		printf("%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d\n", 
-			(packet[0] & 0x1000000000000000) == 1,
-			(packet[0] & 0x0100000000000000) == 1, 
-			(packet[0] & 0x0010000000000000) == 1, 
-			(packet[0] & 0x0001000000000000) == 1, 
-			(packet[0] & 0x0000100000000000) == 1, 
-			(packet[0] & 0x0000010000000000) == 1, 
-			(packet[0] & 0x0000001000000000) == 1, 
-			(packet[0] & 0x0000000100000000) == 1, 
+			(packet->data[0] & 0b1000000000000000) == 1,
+			(packet->data[0] & 0b0100000000000000) == 1,
+			(packet->data[0] & 0b0010000000000000) == 1,
+			(packet->data[0] & 0b0001000000000000) == 1,
+			(packet->data[0] & 0b0000100000000000) == 1,
+			(packet->data[0] & 0b0000010000000000) == 1,
+			(packet->data[0] & 0b0000001000000000) == 1,
+			(packet->data[0] & 0b0000000100000000) == 1,
 			0,
 			0, 
-			(packet[0] & 0x0000000000100000) == 1, 
-			(packet[0] & 0x0000000000010000) == 0, 
-			(packet[0] & 0x0000000000001000) == 1, 
-			(packet[0] & 0x0000000000000100) == 0, 
-			(packet[0] & 0x0000000000000010) == 1, 
-			(packet[0] & 0x0000000000000001) == 1 )
+			(packet->data[0] & 0b0000000000100000) == 1,
+			(packet->data[0] & 0b0000000000010000) == 0,
+			(packet->data[0] & 0b0000000000001000) == 1,
+			(packet->data[0] & 0b0000000000000100) == 0,
+			(packet->data[0] & 0b0000000000000010) == 1,
+			(packet->data[0] & 0b0000000000000001) == 1 )
 	}
 }
 void PacketFilter_PSClassic::filter_setup_packet(SetupPacket* packet,bool direction) {
